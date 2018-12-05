426089992e38d248d4ff2957a6f13ded75ad85a5
pietvanzoen <hi@pietvanzoen.com>
2018-12-05 17:18:40 +0100

    feat: initial server setup

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const pkg = require("./package.json");
+const restify = require("restify");
+const DB = require("./lib/db");
+
+const server = restify.createServer({
+  name: pkg.name,
+  version: pkg.version
+});
+
+const db = new DB();
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get("/v1/plus/:url", function(req, res, next) {
+  res.send(db.findAll());
+  return next();
+});
+
+server.listen(8080, function() {
+  console.log("%s listening at %s", server.name, server.url); // eslint-disable-line
+});
