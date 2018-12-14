b82dc50c32f31afb6035799cdd54e580962cf8f7
pietvanzoen <hi@pietvanzoen.com>
2018-12-14 16:51:12 +0100

    chore: use mktemp for temp dir creation

--- scripts/widget.sh
+++ scripts/widget.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # Compress and minify widget
-
 set -e
+
+TEMP_DIR="$(mktemp -d)"
+
 echo "start minifying..."
 # minify css
-npx postcss widget/plusfries.css > /tmp/plusfries.css
+npx postcss widget/plusfries.css >$TEMP_DIR/plusfries.css
 # compile and minify js app
-npx babel widget/plusfries.js --out-file /tmp/plusfries.js
-npx terser /tmp/plusfries.js -o /tmp/plusfries.js -m -c
+npx babel widget/plusfries.js --out-file $TEMP_DIR/plusfries.js
+npx terser $TEMP_DIR/plusfries.js -o $TEMP_DIR/plusfries.js -m -c
 # inject css
-CSS=`cat /tmp/plusfries.css`
-sed "s/PLUS_FRIES_CSS/${CSS}/g" /tmp/plusfries.js > public/plusfries.js
+CSS=$(cat $TEMP_DIR/plusfries.css)
+sed "s/PLUS_FRIES_CSS/${CSS}/g" $TEMP_DIR/plusfries.js >public/plusfries.js
 # minify widget loader
-npx terser widget/loader.js -o /tmp/loader.js -m -c
-sed "s#PLUS_FRIES_URL#${PLSFRSURL}#g" /tmp/loader.js > public/loader.js
+npx terser widget/loader.js -o $TEMP_DIR/loader.js -m -c
+sed "s#PLUS_FRIES_URL#${PLSFRSURL}#g" $TEMP_DIR/loader.js >public/loader.js
 echo "minification done!"
